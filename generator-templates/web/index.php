489314950147d53fc0ef381c7aa4a9326913b7fd
j.faassen <j.faassen@linkorb.com>
2016-01-24 15:56:32 +0100

    Use autotune in generated index.php

--- generator-templates/web/index.php
+++ generator-templates/web/index.php
@@ -1,9 +1,7 @@
 <?php
 
 $loader = require_once __DIR__.'/../vendor/autoload.php';
-// Uncomment the following lines for local Radvance development
-// $pathToRadvance = __DIR__ .'/../../src/';
-// $loader->addPsr4('Radvance\\', $pathToRadvance, true);
+\AutoTune\Tuner::init($loader);
 
 $app = require_once __DIR__.'/../app/bootstrap.php';
 $app->run();
