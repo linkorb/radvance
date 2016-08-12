7738b0ca1dce0f323a1fa3c00326ba0a60867197
j.faassen <j.faassen@linkorb.com>
2016-08-12 14:20:46 +0200

    Support multiple request logging urls
    - gelf-udp
    - json-path

--- src/Framework/BaseWebApplication.php
+++ src/Framework/BaseWebApplication.php
@@ -128,14 +128,53 @@ abstract class BaseWebApplication extends BaseConsoleApplication implements Fram
             if ($response->headers->has('Content-Type')) {
                 $data['content-type'] = $response->headers->get('content-type');
             }
+            
+            $urls = $this['parameters']['request_log'];
+            foreach (explode(',', $urls) as $url) {
+                $url = trim($url);
+                $url = parse_url($url);
+                //print_r($url);
+                switch ($url['scheme']) {
+                    case 'json-path':
+                        $path = '/' . $url['scheme'] . $url['path'];
 
-            $json = json_encode($data, JSON_PARTIAL_OUTPUT_ON_ERROR|JSON_UNESCAPED_SLASHES);
-            $path = $this->getRootPath() . '/app/logs/requests/' . date('Ymd') . '/';
-            if (!file_exists($path)) {
-                mkdir($path, 0777, true);
+                        $json = json_encode($data, JSON_PARTIAL_OUTPUT_ON_ERROR|JSON_UNESCAPED_SLASHES);
+                        $path = $this->getRootPath() . '/app/logs/requests/' . date('Ymd') . '/';
+                        if (!file_exists($path)) {
+                            mkdir($path, 0777, true);
+                        }
+                        $filename = $path . '/' . date('Ymd-His') . '-' . sha1(rand()) . '.json';
+                        file_put_contents($filename, $json . "\n");
+                        break;
+                        
+                    case 'gelf-udp':
+                        $transport = new \Gelf\Transport\UdpTransport(
+                            $url['host'],
+                            $url['port'],
+                            \Gelf\Transport\UdpTransport::CHUNK_SIZE_WAN
+                        );
+                        $gelfData = $data;
+                        $publisher = new \Gelf\Publisher();
+                        $publisher->addTransport($transport);
+                        $message = new \Gelf\Message();
+                        $message->setShortMessage("Request")
+                            ->setLevel(\Psr\Log\LogLevel::DEBUG)
+                            ->setFullMessage("")
+                            ->setFacility("")
+                            ->setHost($data['host'])
+                        ;
+                        $gelfData['host'] = null;
+                        $gelfData['datetime'] = null;
+                        array_filter($gelfData);
+                        foreach ($gelfData as $key => $value) {
+                            $message->setAdditional($key, $value);
+                        }
+                        $res = $publisher->publish($message);
+                        break;
+                    default:
+                        throw new RuntimeException('Unsupported request_log url scheme: ' . $url['scheme']);
+                }
             }
-            $filename = $path . '/' . date('Ymd-His') . '-' . sha1(rand()) . '.json';
-            file_put_contents($filename, $json);
         });
     }
 
