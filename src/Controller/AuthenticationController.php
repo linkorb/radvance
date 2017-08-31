f1c6632822691fb98e1317ca39af0c0b9734e09f
j.faassen <j.faassen@linkorb.com>
2017-08-31 14:36:55 +0200

    Support custom login template

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -9,7 +9,11 @@ class AuthenticationController
 {
     public function loginAction(Application $app, Request $request)
     {
-        return $app['twig']->render('@BaseTemplates/auth/login.html.twig', array(
+        $templateName = '@Templates/auth/login.html.twig';
+        if (!$app['twig']->getLoader()->exists($templateName)) {
+            $templateName = '@BaseTemplates/auth/login.html.twig';
+        }
+        return $app['twig']->render($templateName, array(
             'error' => $app['security.last_error']($request),
         ));
     }
