1fcb2bd6c363cb5df3c00f4f65e8ef8c6b57685b
Aleksandr <sashaaro@gmail.com>
2019-05-24 18:55:53 +0300

    using psr-15, add HttpKernelMiddleware

--- src/Runner.php
+++ src/Runner.php
@@ -2,6 +2,9 @@
 
 namespace Radvance;
 
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\TerminableInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,11 +17,48 @@ class Runner
         $app = $stack->resolve($app);
             
         $request = $request ?: Request::createFromGlobals();
+        $middlewarePipe = new \Zend\Stratigility\MiddlewarePipe();
 
-        $response = $app->handle($request);
-        $response->send();
+        $psr17Factory = new Psr17Factory();
+        $psrHttpFactory = new PsrHttpFactory($psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory);
+        $psrRequest = $psrHttpFactory->createRequest($request);
+
+        // TODO $middlewarePipe->pipe(\FlexMiddleware\FlexMiddlewareFactory::fromConfig('../config/middlewares.yaml'));
+
+
+        $middlewarePipe->pipe(new HttpKernelMiddleware($app));
+        $psrResponse = $middlewarePipe->handle($psrRequest);
+
+        self::sendResponse($psrResponse);
+
         if ($app instanceof TerminableInterface) {
-            $app->terminate($request, $response);
+            $httpFoundationFactory = new HttpFoundationFactory();
+            $app->terminate($request, $httpFoundationFactory->createResponse($psrResponse));
         }
+    }
+
+    static function sendResponse($response)
+    {
+        //$httpFoundationFactory->createResponse($psrResponse)->send();
+
+        foreach ($response->getHeaders() as $name => $values) {
+            foreach ($values as $value) {
+                header(sprintf('%s: %s', $name, $value), false);
+            }
+        }
+
+        header(sprintf(
+            'HTTP/%s %s %s',
+            $response->getProtocolVersion(),
+            $response->getStatusCode(),
+            $response->getReasonPhrase()
+        ));
+
+        $body = $response->getBody();
+        if ($body->isSeekable()) {
+            $body->rewind();
+        }
+
+        echo $body->getContents();
     }
 }
